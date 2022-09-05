06abbb01475a964158e845625ed874b11e0d27d6
KseniyaSultanova <60780721+KseniyaSultanova@users.noreply.github.com>
2022-09-05 21:01:27 +0300

    ДЗ 3 урока

---
+++ src/test/java/Palindrome/PalindromeTest.java
@@ -0,0 +1,29 @@
+package Palindrome;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+public class PalindromeTest {
+    Palindrome palindrome = new Palindrome();
+
+    @DisplayName("Слово является палиндромом")
+    @ParameterizedTest
+    @ValueSource(strings = {"123321, MOM"})
+    public void TestIsPalindromeMethod(){
+        Assertions.assertTrue(palindrome.isPalindrome("01233210"));
+        Assertions.assertTrue(palindrome.isPalindrome("0123A210"));
+    }
+
+
+    @DisplayName("Слово является палиндромом")
+    @ParameterizedTest
+    @ValueSource(strings = { "racecar", "rAdar", "able was I ere I saw elba" })
+    public void isPalindromeTest(String word){
+        Assertions.assertTrue(palindrome.isPalindrome(word));
+    }
+
+}
